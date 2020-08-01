90568dc7621e1cfafad116a129c3ca0d5a2e0c25
znorio <69048915+znorio@users.noreply.github.com>
2020-08-01 18:18:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # Takes RNA reference and alternative read counts as input
 # Input format should be comparable to the example data files reference_reads.csv and alternative_reads.csv
-# Can either be used to compute posterior distributions of overdispersion terms as well as dropout and mutation probabilities
+
+# The algorithm (more details -> scores.py/calculate_pmat) transfering these read counts to probabilities of mutations depends on several parameters.
+# Two overdispersion terms (overdispersion_mut for the mutated and overdispersion_wt for the non-mutated case) describing the shape of the distributions, 
+# an allelic dropout term (dropout) and the prior probability of mutation (prior_p_mutation) can be optimized with the Metropolis-Hastings algorithm.
+# Additionally the same algorithm tries to derive the phylogenetic relation inbetween single cells to find the tree and parameters, which best 
+# explain the observed read counts. This approach is based on SCIPHI "https://www.nature.com/articles/s41467-018-07627-7".
+
+# It can either be used to compute posterior distributions of the overdispersion terms as well as dropout and mutation probabilities
 # or for the inference of the cell lineage
 
 import numpy as np
@@ -15,6 +22,7 @@ from .scores import calculate_pmat, log_scoretree2
 
 
 # Options
+
 moveProbsParams = [0.25, 0.4, 0.35, 0.05]           # Probability of different moves: 
                                                     # 1.  Range (0,1): Determines the probability that in one round of the Metropolis-Hastings algorithm
                                                     #     the parameters are updated, One minus this probability is the probability that the trees are updated
@@ -23,11 +31,15 @@ moveProbsParams = [0.25, 0.4, 0.35, 0.05]           # Probability of different m
                                                     # 4.  swap subtrees: Swap subtrees only if nodes in different lineages else prune&re-attach
                                                     # 2,3 and 4 are weights -> they don't have to sum up to 1
 
-# For the two overdispersion, dropout and mutation parameters, a prior beta distribution is specified
+# For the two overdispersion, dropout and mutation 
+# parameters, a prior beta distribution is specified.
 priorAlphaBetaoodp = [2, 10, 2, 2, 1.5, 3, 2, 18]   # alpha overdispersion_wt, beta overdispersion_wt, alpha overdispersion_mut, beta overdispersion_mut, 
                                                     # alpha dropout, beta dropout, alpha prior_p_mutation, beta prior_p_mutation
-
 oodp = [100, 1, 0.2, 0.1]                           # overdispersion_wt, overdispersion_mut, dropout, prior_p_mutation
+
+# The covariance matrix is learned adaptively.
+# New parameters are drawn from a multivariate
+# normal distribution.
 covDiagonal = [1, 0.001, 0.0002, 0.00001]           # Initial covariance Matrix is all zeros expcept these values in the diagonal from upper left to lower right
 maxValues = [1000, 2, 1, 1]                         # The maximum values for the parameters overdispersion_wt, overdispersion_mut, dropout, prior_p_mutation
                                                     # Shouldn't be smaller than 1
@@ -35,25 +47,26 @@ minValues = [0,0,0,0]                               # The minimal values for the
 outFile = "tree"                                    # The name of the output files
 rep = 1                                             # number of repetitions of the MCMC
 loops = 100000                                      # number of loops within a MCMC
-gamma = 1                                           # if = 1 no influence
+gamma = 1                                           # if = 1 -> no influence
 initialPeriod = 10000                               # number of iterations before the initial covariance matrix is adapted                               
 sampleStep = 1                                      # stepsize between sampling of parameters and trees/ 1 -> sampled every round, 2 -> sampled every second round,...
 burnInPhase = 0.25                                  # burnIn / total number of loops
 decVar = 0.1                                        # The covariance matrix is multiplied with this factor, to increase or decrease it 0.1 -> 10 times smaller
                                                     # increases or decreases the acceptance rate
-adaptAcceptanceRate = True                          # if true starts with given decVar, but adapts it every 1000 steps if the acceptance rate lies outside 1/4 to 1/2
+adaptAcceptanceRate = True                          # if true starts with given decVar, but adapts it every 1000 steps, if the acceptance rate lies outside 1/4 to 1/2
 factorParamsLogScore = 10                           # Is multiplied with the parameter log score to increase or decrease its influence compared to the tree log score
-                                                    # -> Helps to prevent empty tree solution
+                                                    # -> Helps to prevent empty tree solution (all cells attached to the root).
 factor_owt = 2                                      # Is additionally multiplied with the overdisperison_wt log-score, because this is the main parameter 
-                                                    # responsible for the empty tree solution
-marginalization = True                              # If false the program maximizes the placement of the cells, if true the program tries to find the marginal distribution 
-                                                    # of the parameters
+                                                    # responsible for the empty tree solution.
+marginalization = True                              # If false the program maximizes the placement of the cells, if true the program tries to find the marginal 
+                                                    # distribution of the parameters.
 
 optTreeOutputNum = 3                                # Determines the maximal number of optimal trees for the output, if output_gv = True and / or output_mut_csv = True
-                                                    # if = -1 no restrictions on the output size
+                                                    # if = -1 -> no restrictions on the output size
 output_mut_csv = True                               # if true outputs 1,0 mutation matrix of optimal trees / marginalization should be False
 output_gv = True                                    # if true outputs graphviz file of optimal trees / marginalization should be False
-output_samples = True                               # if true outputs all samples after burn-in of current tree log-score, current params and curent parent vector as numpy array
+output_samples = True                               # if true outputs all samples after burn-in of current tree log-score,
+                                                    # current params and curent parent vector as numpy array
 output_ProbabilityMatrix = True                     # if true outputs probability matrix of best parameters as csv file
 
 path = ".Data/"                                     # Specify the main path to the files
@@ -73,7 +86,7 @@ alt = np.array(pd_alt.fillna(0)).tolist()
 
 # Determine number of cells and mutation sites
 rows_ref = len(ref)
-num_mut = rows_ref
+num_mut = rows_ref # number of mutations is equal to the number of rows, because the cellnames are removed
 
 columns_ref = len(ref[1])
 num_cells = columns_ref - 1
@@ -97,7 +110,7 @@ samples, sampleParams, optimal, bestParams = runMCMCoodp(rep, loops, oodp, prior
 
 # create all desired output files
 
-# probability matrix of mutation using best parameters
+# Uses the best parameters to calculate the probability of mutation using the RNA read counts
 if output_ProbabilityMatrix == True:           
     pmat = calculate_pmat(bestParams[0], bestParams[1], bestParams[2], bestParams[3])
     savetxt(outFile + "_pmat.csv", pmat, delimiter = ",")
@@ -115,7 +128,7 @@ if output_mut_csv == True:
         savetxt(outFile + "_mut_" + str(o) + ".csv", mut, delimiter = ',')
         
 
-# if true outputs graphviz file of tree
+# if true outputs graphviz file of optimal trees
 if output_gv == True:
     if optTreeOutputNum == -1:
         optTreeOutputNum = len(optimal)
