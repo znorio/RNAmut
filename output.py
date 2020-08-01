100742b4d4f39215dfd4039a335405e48d193d7f
znorio <69048915+znorio@users.noreply.github.com>
2020-08-02 00:15:32 +0200

    Update output.py

--- output.py
+++ output.py
@@ -1,23 +1,39 @@
-import numpy as np
+# Functions used to create the desired output files
 
-def getAttachmentPoints(parVec, Params, n, m):
+import numpy as np
+from .scores import calculate_pmat
+from .trees import parentVector2ancMatrix
+
+# Determines the optimal attachment points of the cells to the mutation tree
+def getAttachmentPoints(parVec, Params, num_mut, num_cells, frequency_of_nucleotide, sequencing_error_rate):
+    """
+    Args:
+        parVec                  - Parent vector (list)
+        Params                  - [overdispersion_wt, overdispersion_mut, dropout, prior_p_mutation] (list)
+        num_mut                 - Number of mutation sites (int)
+        num_cells               - Number of cells (int)
+        frequency_of_nucleotide - Expected allele frequency (float)
+        sequencing_error_rate   - Sequencing error rate (float)
         
-    pmat = calculate_pmat(Params[0], Params[1], Params[2], Params[3])
+    Returns:
+        Optimal attachment points (list)
+    """    
+    pmat = calculate_pmat(Params[0], Params[1], Params[2], Params[3], frequency_of_nucleotide, sequencing_error_rate )
     
     log_pmat_m = np.log(pmat)
     log_pmat_r = np.log(1 - pmat)
     
     attachmentPoints = []
     
-    children = [[] for v in range(n + 1)]
-    for q in range(n):
+    children = [[] for v in range(num_mut + 1)]
+    for q in range(num_mut):
         children[parVec[q]].append(q) 
 
-    bf = [0] * (n + 1)
-    bf[0] = n
+    bf = [0] * (num_mut + 1)
+    bf[0] = num_mut
     z = 0
 
-    for w in range(n + 1):
+    for w in range(num_mut + 1):
         for t in range(len(children[bf[w]])):
             z += 1
 
@@ -25,10 +41,10 @@ def getAttachmentPoints(parVec, Params, n, m):
     
     for i in range(m):
         
-        score = [0] * (n + 1)
+        score = [0] * (num_mut + 1)
         
         for j in range(num_mut):
-            score[n] += log_pmat_r[j,i] 
+            score[num_mut] += log_pmat_r[j,i] 
         
         for k in range(1, num_mut + 1):
                        
@@ -42,37 +58,62 @@ def getAttachmentPoints(parVec, Params, n, m):
     return attachmentPoints
   
     
-def oneZeroMut(Params, parVec, n, m):
+# Calculates a one zero mutation matrix
+# All cells are optimally attached to the muatation tree
+def oneZeroMut(Params, parVec, num_mut, num_cells, frequency_of_nucleotide, sequencing_error_rate):
+    """
+    Args:
+        Params                  - [overdispersion_wt, overdispersion_mut, dropout, prior_p_mutation] (list)
+        parVec                  - Parent vector of tree (list)
+        num_mut                 - Number of mutation sites (int)
+        num_cells               - Number of cells (int)
+        frequency_of_nucleotide - Expected allele frequency (float)
+        sequencing_error_rate   - Sequencing error rate (float)
+        
+    Returns:
+        One zero matrix (numpy array)
+    """    
+    ancMatrix = parentVector2ancMatrix(parVec, num_mut)
     
-    ancMatrix = parentVector2ancMatrix(parVec, n)
-    
-    attachmentPoints = getAttachmentPoints(parVec, Params, n, m)
+    attachmentPoints = getAttachmentPoints(parVec, Params, num_mut, num_cells, frequency_of_nucleotide, sequencing_error_rate)
 
-    mat = np.zeros((n, m))
+    mat = np.zeros((num_mut, num_cells))
 
     for m, j in enumerate(attachmentPoints):
-        if(j < n):
-            for k in range(n):
+        if(j < num_mut):
+            for k in range(num_mut):
                 if (ancMatrix[k][j] == 1):
                     mat[k][m] = 1
 
     return mat
   
 
-def graphviz(Params, parentVector, n, m):
-
+# Creates a graphviz file
+def graphviz(Params, parVec, num_mut, num_cells, frequency_of_nucleotide, sequencing_error_rate):
+    """
+    Args:
+        Params                  - [overdispersion_wt, overdispersion_mut, dropout, prior_p_mutation] (list)
+        parVec                  - Parent vector of tree (list)
+        num_mut                 - Number of mutation sites (int)
+        num_cells               - Number of cells (int)
+        frequency_of_nucleotide - Expected allele frequency (float)
+        sequencing_error_rate   - Sequencing error rate (float)
+        
+    Returns:
+        Graphviz file (string)
+    """    
     gene_names.append("Root")
     
     gv = "digraph G {\n"
     gv += "node [color=deeppink4, style=filled, fontcolor=white];\n"
 
-    for i in range(n):
+    for i in range(num_mut):
         # The parantheses around the gene names help displaying them if they contain special characters
-        gv += ("\"" + gene_names[parentVector[i]] + "\"" + " -> "  + "\"" + gene_names[i]  + "\"" + ";\n" ) 
+        gv += ("\"" + gene_names[parVec[i]] + "\"" + " -> "  + "\"" + gene_names[i]  + "\"" + ";\n" ) 
 
     gv += "node [color=lightgrey, style=filled, fontcolor=black];\n"
                             
-    attachmentPoints = getAttachmentPoints(parentVector, Params, n, m)
+    attachmentPoints = getAttachmentPoints(parVec, Params, num_mut, num_cells, frequency_of_nucleotide, sequencing_error_rate)
     
     for y, a in enumerate(attachmentPoints):
         gv += "\"" + gene_names[a] + "\"" + " -> s"  + str(y + 1) + ";\n"
