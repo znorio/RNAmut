0b5e5f62ba2dd2d3b31a1749e682977317c9ca8a
znorio <69048915+znorio@users.noreply.github.com>
2020-08-02 17:09:41 +0200

    Update scores.py

--- scores.py
+++ scores.py
@@ -61,12 +61,34 @@ def calculate_pmat(overdispersion_wt, overdispersion_mut, dropout, prior_p_mutat
     return pmat
 
 
+# Logarithm of probability density function (pdf) of the beta distribution
 def log_pdf(a,b,x):
+    """
+    Args:
+        a - alpha (float)
+        b - beta (float)
+        x - range: (0,1) / probability density is determined at point x (float)
+        
+    Returns:
+        Logarithm of pdf (float)
+    """
     return math.lgamma(a + b) - math.lgamma(a) - math.lgamma(b) + (a - 1) * math.log(x) + (b - 1) * math.log(1 - x)
 
 
-def log_scoretree2(pmat, parVec):
-    
+# logarithmic tree score
+# Uses the logarithmic scoring rule to determine the difference between the calculated probabilities of mutation 
+# and the one zero mutation probabilities derived from the mutation tree
+# marginalization of the attachment points is possible (to get posterior distributions of the learnable parameters)
+def log_scoretree(pmat, parVec, marginalization):
+     """
+    Args:
+        pmat            - calculated probabilities of mutation (numpy array)
+        parVec          - parent vector of tree (list)
+        marginalization - if true the attachment points are marginalized (bool)
+        
+    Returns:
+        Logarithmic tree score (float)
+    """
     log_pmat_m = np.log(pmat)
     log_pmat_r = np.log(1 - pmat)
     
@@ -83,7 +105,8 @@ def log_scoretree2(pmat, parVec):
     for w in range(num_mut + 1):
         for t in range(len(children[bf[w]])):
             z += 1
-            bf[z] = children[bf[w]][t]           # determine where the mutations are in the tree to later be able to add the log score from root to bottom of the tree
+            bf[z] = children[bf[w]][t]           # determine where the mutation_sites are located in the tree 
+                                                 # to later be able to add the log score from root to bottom of the tree
     
     for i in range(num_cells):
         
@@ -97,60 +120,38 @@ def log_scoretree2(pmat, parVec):
             node = bf[k]
 
             score[node] = score[parVec[node]]
-            score[node] -= log_pmat_r[node,i]     # step by step the mutation log score is added and because the mutation replaces a reference, the reference log score is substracted
-            score[node] += log_pmat_m[node,i]
+            score[node] += log_pmat_m[node,i]   # step by step the mutation log score is added 
+            score[node] -= log_pmat_r[node,i]   # because the mutation replaces a reference, the reference log score is substracted
 
-        log_score += max(score)
+        if marginalization == False:
+            log_score += max(score)
+            
+        if marginalization == True:
+            log_score += max(score) + math.log(sum(math.e ** (score - max(score))))
 
     return log_score
 
-if marginalization == True:
-    
-    def log_scoretree2(pmat, parVec):
 
-        log_pmat_m = np.log(pmat)
-        log_pmat_r = np.log(1 - pmat)
-
-        log_score = 0
-
-        children = [[] for v in range(num_mut + 1)]
-        for q in range(num_mut):
-            children[parVec[q]].append(q) 
-
-        bf = [0] * (num_mut + 1)
-        bf[0] = num_mut
-        z = 0
-
-        for w in range(num_mut + 1):
-            for t in range(len(children[bf[w]])):
-                z += 1
-                bf[z] = children[bf[w]][t]           # determine where the mutations are in the tree to later be able to add the log score from root to bottom of the tree
-
-        for i in range(num_cells):
-
-            score = [0] * (num_mut + 1)
-
-            for j in range(num_mut):
-                score[num_mut] += log_pmat_r[j,i] 
-
-            for k in range(1, num_mut + 1):
-
-                node = bf[k]
-
-                score[node] = score[parVec[node]]
-                score[node] -= log_pmat_r[node,i]     # step by step the mutation log score is added and because the mutation replaces a reference, the reference log score is substracted
-                score[node] += log_pmat_m[node,i]
-
-            log_score += max(score) + math.log(sum(math.e ** (score - max(score))))
-
-        return log_score
-
-def log_scoreparams(Params, maxValues, ab):
-
+# logarithmic parameter score
+# The factorParamsLogScore determines how strongly the prior parameter distributions affect the combined tree and parameters log score
+# Because a small overdispersion_wt parameter might lead to an empty tree solution, meaning none of the cells are predicted to have a mutation, 
+# it is possible to strengthen the prior for this parameter with the factor_owt.
+def log_scoreparams(params, maxValues, priorAlphaBetaoodp, factor_owt, factorParamsLogScore):
+    """
+    Args:
+        params               - [overdispersion_wt, overdispersion_mut, dropout, prior_p_mutation] (list)
+        maxValues            - maximum values of the parameters (list)
+        priorAlphaBetaoodp   - prior alphas and betas of the parameter beta distributions (list)
+        factor_owt           - strengthens/(weakens) prior of overdispersion_wt (int/float)
+        factorParamsLogScore - strengthens/weakens priors (int/float)
+        
+    Returns:
+        Logarithmic parameters score (float)
+    """
     log_score = 0            
-    log_score += log_pdf(ab[0], ab[1], Params[0] / maxValues[0]) * factor_owt
-    log_score += log_pdf(ab[2], ab[3], Params[1] / maxValues[1])
-    log_score += log_pdf(ab[4], ab[5], Params[2] / maxValues[2])
-    log_score += log_pdf(ab[6], ab[7], Params[3] / maxValues[3])
+    log_score += log_pdf(priorAlphaBetaoodp[0], priorAlphaBetaoodp[1], params[0] / maxValues[0]) * factor_owt
+    log_score += log_pdf(priorAlphaBetaoodp[2], priorAlphaBetaoodp[3], params[1] / maxValues[1])
+    log_score += log_pdf(priorAlphaBetaoodp[4], priorAlphaBetaoodp[5], params[2] / maxValues[2])
+    log_score += log_pdf(priorAlphaBetaoodp[6], priorAlphaBetaoodp[7], params[3] / maxValues[3])
     
     return log_score * factorParamsLogScore
